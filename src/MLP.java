be2799cc3f1c60e8d34d98aca480ba46a5b5f9ee
Veccher <Veccher@Veccher-PC>
2018-09-20 22:59:08 -0300

    Commit inicial

---
+++ src/MLP.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+import weka.classifiers.functions.MultilayerPerceptron;
+import weka.core.Instances;
+import weka.core.Utils;
+import weka.core.converters.ArffLoader.ArffReader;
+
+public class MLP implements Runnable{
+	Instances data;
+	private String options;
+	public MultilayerPerceptron model;
+	public float precision;
+	
+	private void setOptions(ArrayList<Integer> layers) {
+		options=new String("-H ");
+		boolean first=true;
+		for (Integer layerSize:layers){
+			if(!first)
+				options+=",";
+			options+=layerSize.toString();
+			first=false;
+		}
+		if (options.equals("-H"))
+			options="";
+		//System.out.println(options);
+	}
+	public MLP (ArrayList<Integer> layers) throws IOException
+	{
+		BufferedReader reader =new BufferedReader(new FileReader("iris.arff"));
+		ArffReader arff = new ArffReader(reader);
+		this.data = arff.getData();
+		data.setClassIndex(data.numAttributes() - 1);
+		setOptions(layers);
+		//new Thread(this).start();
+	}
+	public synchronized Float classify() throws Exception {
+		int acertoTotal=0;
+		int testeTotal=0;
+		for (int i=0;i<10;i++)
+		{
+			data.randomize(new java.util.Random(0));
+			//int acertosCamada=0;
+			int j=0;
+			Instances trainSet=data.trainCV(10,i);
+			Instances testSet=data.testCV(10,i);
+			model = new MultilayerPerceptron();
+			((MultilayerPerceptron) model).setOptions(Utils.splitOptions(options));
+			model.buildClassifier(trainSet);
+			
+			testeTotal+=testSet.size();
+			for (j=0;j<testSet.size();j++)
+			{
+				if(model.classifyInstance(testSet.get(j))==testSet.get(j).classValue())
+				{
+					//acertosCamada++;
+					acertoTotal++;
+				}
+			}
+			
+			//System.out.print("taxa de acerto no conjunto="+acertosCamada*100f/j+"%\n");
+				
+		}
+		this.precision=acertoTotal*100f/testeTotal;
+		notify();
+		return acertoTotal*100f/testeTotal;	
+	}
+	float getResults()
+	{
+		return this.precision;
+	}
+	@Override
+	public void run() {
+		try {
+			this.precision=this.classify();
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public int compareTo(MLP other) {
+		return ((Float)other.getResults()).compareTo(this.getResults());
+	}
+}
---
+++ src/SimulatedAnnealing.java
@@ -0,0 +1,80 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class SimulatedAnnealing {
+	float heat = 100;
+	float decayRate = 3;
+	boolean isRandom()
+	{
+		Random rand=new Random();
+		heat-=decayRate;
+		if (heat<rand.nextInt()%100)
+			return false;
+		return true;
+	}
+	public static void main(String[] args)
+	{
+		int nLayers=3;
+		Random rand=new Random();
+		float top=0;
+		ArrayList<Integer> currentArch = new ArrayList<Integer>();
+		//random initialize
+		/*for (int j=0;j<60;j++)
+		{
+			currentArch = new ArrayList<Integer>();*/
+		for(int i=0;i<nLayers;i++)
+		{
+			currentArch.add(rand.nextInt(10)+1);
+		}/*
+			try {
+				MLP net=new MLP(currentArch);
+				System.out.println(j+ " " + net.classify());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}*/
+		//iterate
+		try {
+			top=new MLP(currentArch).classify();
+			while(true)
+			{
+				float temp=0;
+				ArrayList<Integer> tempArch = new ArrayList<Integer>();
+				ArrayList<Integer> temp2Arch = new ArrayList<Integer>();
+				System.out.println("Arch Atual: "+currentArch+"precisão: "+top);
+				for (int camada=0;camada<currentArch.size();camada++)
+				{
+					for (int add=-1;add<2;add+=2)
+					{
+						temp2Arch.clear();
+						temp2Arch.addAll(currentArch);
+						temp2Arch.set(camada, temp2Arch.get(camada)+add);
+						MLP net=new MLP(temp2Arch);
+						System.out.println("camada "+camada+" incremento: "+add+" arq: "+temp2Arch+" precisao: " +net.classify());
+						if(net.getResults()>temp)
+						{
+							temp=net.getResults();
+							tempArch.clear();
+							tempArch.addAll(temp2Arch);
+						}
+					}
+				}
+				if (temp<=top)
+					break;
+				top=temp;
+				currentArch=tempArch;
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
